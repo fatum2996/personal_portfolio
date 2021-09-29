60a0692385ad4d8d598714c6da0dde82576cca5d
fatum <fa-tum@yandex.ru>
2021-09-29 22:24:08 +0300

    saved views with index in blog

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,5 @@ from .models import Blog_record
 
 # Create your views here.
 def all_blogs(request):
-    blogs = Blog_record.objects.order_by('-data')
+    blogs = Blog_record.objects.order_by('-data')[:5]
     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
